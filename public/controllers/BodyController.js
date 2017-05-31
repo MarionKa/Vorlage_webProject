d32f62588774c113d4750b01ec034f33c7e21320
michicmj <cmj2@live.de>
2017-05-31 16:19:31 +0200

    alles mögliche
    passport
    combobox
    unnützes gelöscht

--- public/controllers/BodyController.js
+++ public/controllers/BodyController.js
@@ -1,4 +1,9 @@
-﻿webTestDB.controller("BodyController", function ($scope) {
+﻿webTestDB.controller("BodyController", function ($scope,AuthService) {
 
-    $scope.date = new Datum().dateString();
+    // $scope.date = new Datum().dateString();
+    $scope.logout = function() {
+    	console.log('LOGOUT');
+    	AuthService.logout();
+
+    };
 });
--- public/scripts/Datum.js
+++ public/scripts/Datum.js
@@ -1,32 +1,35 @@
-﻿function Datum(date) {
+﻿//BRAUCHEN WIR DES ?
 
-    var myDate = date;
 
-    this.toDateString = function () {
-        var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-        return (new Intl.DateTimeFormat('de-DE', options)).format(myDate);
-    }
+// function Datum(date) {
 
-    this.fromDateString = function (dateString) {
-        var regEx = /(\d{2})\.(\d{2})\.(\d{4})/;
-        var dateElements = regEx.exec(dateString);
-        myDate = new Date(
-            (+dateElements[3]),
-            (+dateElements[2]) - 1,// Vorsicht, Monat startet bei 0!
-            (+dateElements[1]));
-        return this;
-    }
+//     var myDate = date;
 
-    this.dateString = function (newDateString) {
-        if (newDateString)
-            this.fromDateString(newDateString);
-        return this.toDateString();
-    }
+//     this.toDateString = function () {
+//         var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+//         return (new Intl.DateTimeFormat('de-DE', options)).format(myDate);
+//     }
 
-    this.date = function (newDate) {
-        if (newDate)
-            myDate = newDate;
-        return myDate;
-    }
-}
+//     this.fromDateString = function (dateString) {
+//         var regEx = /(\d{2})\.(\d{2})\.(\d{4})/;
+//         var dateElements = regEx.exec(dateString);
+//         myDate = new Date(
+//             (+dateElements[3]),
+//             (+dateElements[2]) - 1,// Vorsicht, Monat startet bei 0!
+//             (+dateElements[1]));
+//         return this;
+//     }
+
+//     this.dateString = function (newDateString) {
+//         if (newDateString)
+//             this.fromDateString(newDateString);
+//         return this.toDateString();
+//     }
+
+//     this.date = function (newDate) {
+//         if (newDate)
+//             myDate = newDate;
+//         return myDate;
+//     }
+// }
 
